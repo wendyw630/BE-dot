9620a5c6bd52779929eb4daf712df88546c79407
wangzl630 <57745474+wendyw630@users.noreply.github.com>
2022-04-06 16:24:36 +0800

    Create baseEditorsTable.py

---
+++ designsgRNA/baseEditorsTable.py
@@ -0,0 +1,82 @@
+CBElist={
+    #name: [ PAM, major activity window, start ,end]
+    "BE1": ["NGG",13,17,"C","T", "U"],
+    "BE2": ["NGG",13,17,"C","T","U"],
+    "BE3": ["NGG",13,17,"C","T","U"],
+    "HF-BE3": ["NGG",13,17,"C","T","U"],
+    "BE4(max)": ["NGG",13,17,"C","T","U"],########  v
+    "BE4-Gam": ["NGG",13,17,"C","T","U"],
+    "YE1-BE3": ["NGG",14,16,"C","T","U"],
+    "YEE-BE3": ["NGG",15,15,"C","T","U"],
+    "VQR-BE3": ["NGAN",10,17,"C","T","U"],
+    "VRER-BE3": ["NGCG",11,18,"C","T","U"],
+    "SaBE3": ["NNGRRT",10,19,"C","T","U"],
+    "SaBE4": ["NNGRRT",10,19,"C","T","U"],
+    "SaBE4-Gam": ["NNGRRT",10,19,"C","T","U"],
+    "Sa(KKH)-BE3": ["NNNRRT",10,19,"C","T","U"],
+    #"Cas12a-BE": ["TTTV",10,12,"C","T","D"], #### wzl_note: D
+    "Target-AID": ["NGG",17,19,"C","T","U"],
+    "Target-AID-NG": ["NG",17,19,"C","T","U"],
+    "xBE3": ["NG",13,17,"C","T","U"],
+    "eA3A-BE3": ["NGG",13,17,"C","T","U"], #special case, only when C comes after T
+    "BE-PLUS": ["NGG",7,17,"C","T","U"],
+    "CP-CBEmax variants": ["NGG",8,17,"C","T","U"], ## wzl_changed wind
+    "evoAPOBEC1-BE4max": ["NGG",13,18,"C","T","U"],
+    "evoFERNY-BE4max": ["NGG",13,18,"C","T","U"],
+    "evoCDA1-BE4max": ["NGG",9,20,"C","T","U"],
+    "SpRY-BE4max":["NRN",12,18,"C","T","U"], ### wzl_add,3-9site   vvvvv
+    "SpRY-PmCDA1":["NRN",15,20,"C","T","U"] ### wzl_add,1-6site,[plant]
+
+}
+
+ABElist={
+    #name: [ PAM, major activity window, start ,end]
+    "ABE 7.9": ["NGG",13,16, "A","G","U"],
+    "ABE 7.10": ["NGG",14,17, "A","G","U"],  ###### ABEmax  vvvvvvvvv
+    "ABE 7.10*": ["NGG",13,17, "A","G","U"],
+    "xABE":["NG",14,17, "A","G","U"],
+    "NG-ABEmax": ["NG",14,17, "A","G","U"],
+    "ABESa": ["NNGRRT",10,16, "A","G","U"],
+    "VQR-ABE": ["NGA",15,17, "A","G","U"],
+    "VRER-ABE": ["NGCG",15,17, "A","G","U"],
+    "Sa(KKH)-ABE": ["NNNRRT",10,16, "A","G","U"],
+    "ABE8e":["NGG",13,17,"A","G","U"], ## wzl_add
+    "CP-ABEmax variants":["NGG",11,17, "A","G","U"],##  wzl_changed wind
+    "SpRY-ABE8e":["NRN",13,17,"A","G","U"], ## wzl_add,4-8site,[plant] ########## vvvvvv
+
+}
+
+CGBElist={
+
+    "CG-689":["NGG",13,17, "C","G","U"],
+    "CG-X-689-X-RBMX":["NGG",11,17, "C","G","U"],
+    #"eA3A-nCas9"
+    #"RBMX-eA3A-UdgX-HF-nCas9"
+    "CG-RBMX-eA3A-X":["NGG",12,15, "C","G","U"],
+    "CG-EE":["NGG",14,16, "C","G","U"],
+    "CG-X-EE-X-X":["NGG",13,17, "C","G","U"],
+    "CG-APOBEC1":["NGG",13,16, "C","G","U"],
+    "CG-X-APOBEC1-X-HF":["NGG",10,16, "C","G","U"],
+    "CG-POLD2-APOBEC1-X":["NGG",11,16, "C","G","U"]
+
+}
+
+BEletter={
+                "N":["A","C","T","G"],
+                "A":["A"],
+                "C":["C"],
+                "T":["T"],
+                "G":["G"],
+                "U":["U"],
+                "W": ["A","T"],
+                "S": ["C","G"],
+                "M": ["A","C"],
+                "K": ["G","T"],
+                "R": ["A","G"],
+                "Y": ["C","T"],
+                "B": ["C","G","T"],
+                "D": ["A","G","T"],
+                "H": ["A","C","T"],
+                "V": ["A","C","G"],
+
+}
